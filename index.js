65c4e4d834827b491c795ff1a4201a1b5e2af5e5
victor-kidiga <you@example.comvictorkidiga680@gmail.com>
2026-04-13 09:22:25 +0300

    lab

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 
+function mergeUsernames(...usernameArrays) {
+  const allUsernames = [].concat(...usernameArrays);
+  return {
+    usernames: allUsernames,
+  };
+}
 
+require('datejs');
+
+function combineUsers(...args) {
+  
+  const combinedObject = {
+    users: []
+  };
+
+  args.forEach(individualArray => {
+    
+    combinedObject.users = [...combinedObject.users, ...individualArray];
+  });
+
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
